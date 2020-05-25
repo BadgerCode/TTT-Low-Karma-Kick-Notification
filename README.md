edadbdb2d34c9fdd5e0b3dd281d77deacd225ae8
Michael Hawkins <BadgerCode@users.noreply.github.com>
2020-05-25 13:34:45 +0100

    Adds discord notifications

--- README.md
+++ README.md
@@ -5,6 +5,17 @@ Sends an in-game notification when a player is kicked/banned from a game of Garr
 
 [TTT Karma configuration options](http://ttt.badking.net/config-and-commands/convars#TOC-Karma)
 
+<br>
+
+## Discord
+The notification can also be send to a [Discord Webhook](https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks).<br>
+In your `server.cfg`, set `ttt_karmakick_discordwebhookurl` to your Discord webhook URL.
+
+**IMPORTANT**: Unfortunately, Discord's servers block notifications from Garry's Mod servers.<br>
+You will need to proxy your requests through another website.
+
+<br><br>
+
 
 ## Debugging
 To debug, add the following lines to `lua/autorun/server/karma_kick_notification.lua`
--- lua/autorun/server/karma_kick_notification.lua
+++ lua/autorun/server/karma_kick_notification.lua
@@ -1,4 +1,6 @@
-KarmaKickNotification = {}
+KarmaKickNotification = {
+    DiscordWebhookURL = CreateConVar("ttt_karmakick_discordwebhookurl", "", FCVAR_PROTECTED + FCVAR_LUA_SERVER, "Discord Webhook URL for karma kick notifications")
+}
 
 hook.Remove("PlayerDisconnected", "KarmaKickNotif_PlayerDisconnected")
 hook.Add("PlayerDisconnected", "KarmaKickNotif_PlayerDisconnected", function(ply)
@@ -21,4 +23,49 @@ KarmaKickNotification.SendNotification = function(kickedPlayer)
     for k, ply in pairs(player.GetAll()) do
         ply:ChatPrint(chatMessage)
     end
+
+    local discordWebhookURL = KarmaKickNotification.DiscordWebhookURL:GetString()
+    if(discordWebhookURL != "") then
+        KarmaKickNotification.SendDiscordNotification(discordWebhookURL, chatMessage)
+    end
+end
+
+
+local limit = 5
+local reset = 0
+KarmaKickNotification.SendDiscordNotification = function(webhookURL, message)
+    local now = os.time(os.date("!*t"))
+
+    if limit == 0 and now < reset then
+        local function tcb()
+            KarmaKickNotification.SendDiscordNotification(webhookURL, message)
+        end
+
+        timer.Simple(reset - now, tcb)
+    end
+
+    HTTP({
+        method = "POST",
+        url = webhookURL,
+        headers = {
+            accept = "application/json"
+        },
+        body = util.TableToJSON({
+            content = message
+        }),
+        type = "application/json",
+        success = function(status, body, headers)
+            if(status == 403) then
+                print("Error calling discord webhook for karma kick- access denied.\nGarry's Mod servers are not allowed to directly call Discord Webhooks.\nPlease use a proxy and check your URL for mistakes.")
+            elseif(status < 200 or status > 299) then
+                print(string.format("Error calling discord webhook for karma kick (check data/discord-kick-notification-errors.txt)- \"%s\".", tostring(status), body))
+                file.Append("discord-kick-notification-errors.txt", string.format("%s\n%s\n\n\n", tostring(status), body))
+            end
+            limit = headers["X-RateLimit-Remaining"]
+            reset = headers["X-RateLimit-Reset"]
+        end,
+        failed = function(reason)
+            print(string.format("Error calling discord webhook for karma kick- \"%s\"", reason))
+        end
+    })
 end
